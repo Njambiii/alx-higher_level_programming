d908a47fc93b09855d97e6184f6f6d74c9843089
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-11-30 03:24:55 +0300

    Delete 9-print_last_digit.py

--- 9-print_last_digit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def print_last_digit(number):
-    if number < 0:
-        number *= -1
-    lastDigit = number % 10
-
-    print("{}".format(lastDigit), end="")
-    return lastDigit
