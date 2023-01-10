b41d604b4403109bd6671fb37304bf780599cb0b
Njambiii <111266752+Njambiii@users.noreply.github.com>
2023-01-10 23:42:56 +0300

    Task 10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module ``10-student``"""
+
+
+class Student:
+    """A class for Students"""
+
+    def __init__(self, first_name, last_name, age):
+        """Inilializes the first, last names, and the age of the student
+        Args:
+            first_name (str): First name of the student
+            last_name (str): Last name of the student
+            age (int): Age of th student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+
+        a_dict = vars(self)
+        if type(attrs) == list and all(isinstance(i, str) for i in attrs):
+            new_dict = {}
+            for key in filter(lambda key: key in a_dict, attrs):
+                new_dict[key] = a_dict[key]
+            return new_dict
+
+        return a_dict
