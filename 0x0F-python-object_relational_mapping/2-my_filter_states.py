b01048c8f19ff3d90e8450ceafbde8daa77d51e7
Njambiii <111266752+Njambiii@users.noreply.github.com>
2023-04-28 02:54:19 +0300

    Task 2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
+"""
+    script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+"""
+from sys import argv
 import MySQLdb
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    try:
+        search = argv[4]
+        stmt = """
+        SELECT * FROM states WHERE name LIKE BINARY '{:s}' ORDER BY id ASC;
+        """.format(search)
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
+    cur.close()
+    conn.close()
