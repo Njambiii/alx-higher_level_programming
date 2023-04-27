32ef61f8b6e2c660b52623d529298689e74a41d8
Njambiii <111266752+Njambiii@users.noreply.github.com>
2023-04-28 02:50:50 +0300

    Task 0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
+""" script that lists all states from the database hbtn_0e_0_usa """
+from sys import argv
 import MySQLdb
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    try:
+        stmt = "SELECT * FROM states ORDER BY id ASC"
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
+    cur.close()
+    conn.close()
